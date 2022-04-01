8a4828a8e9ebc01a2da8a0ca1a0d2ac492f8242d
Mirkoweitkowitz <99251369+Mirkoweitkowitz@users.noreply.github.com>
2022-04-01 17:14:24 +0200

    mirko war hier

--- js/lektion1/ArtikelTag.js
+++
@@ -1,9 +0,0 @@
-class ArtikelTag extends React.Component {
-    render = () => {
-        return (
-            <div>
-                <dd><label><input type="checkbox"/> Artikel</label></dd>
-            </div>
-        )
-    }
-}
